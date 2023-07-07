49a7ef6e6c89174a680f655fdd98a61bff482d32
Md Juhaer Adittya Pasha <juhaeradittya2000@gmail.com>
2023-07-07 13:41:30 +0000

    Done with Homework1 Week05

--- code/Week05/homework/Homework1.hs
+++ code/Week05/homework/Homework1.hs
@@ -1,21 +1,40 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Homework1 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy, POSIXTime,
-                                       PubKeyHash, ScriptContext,
-                                       mkMintingPolicyScript)
-import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (False), ($))
-import           Utilities            (wrapPolicy)
+import           Data.Maybe                (fromJust)
+import           Plutus.V1.Ledger.Interval (contains, overlaps, to, from)
+import           Plutus.V2.Ledger.Api      (BuiltinData, POSIXTime, TxInfo (txInfoValidRange), CurrencySymbol,
+                                            MintingPolicy, PubKeyHash,
+                                            ScriptContext (scriptContextTxInfo),
+                                            mkMintingPolicyScript)
+import           Plutus.V2.Ledger.Contexts (txSignedBy)
+import           PlutusTx.Builtins         
+import qualified PlutusTx                  
+import           PlutusTx.Prelude          (Bool, traceIfFalse, ($), (.), not, (&&))
+import           Prelude                   (IO, Show (show))
+import           Text.Printf               (printf)
+import           Utilities                 (currencySymbol, wrapPolicy,
+                                            writeCodeToFile, writePolicyToFile)
 
 {-# INLINABLE mkDeadlinePolicy #-}
 -- This policy should only allow minting (or burning) of tokens if the owner of the specified PubKeyHash
 -- has signed the transaction and if the specified deadline has not passed.
 mkDeadlinePolicy :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkDeadlinePolicy _pkh _deadline () _ctx = False -- FIX ME!
+mkDeadlinePolicy _pkh _deadline () _ctx = ((traceIfFalse "signature missing" signedByBeneficiary) &&
+                                (traceIfFalse "deadline not reached" deadlineNotReached))
+                                where
+                                     info :: TxInfo
+                                     info = scriptContextTxInfo _ctx
+
+                                     signedByBeneficiary :: Bool
+                                     signedByBeneficiary = txSignedBy info _pkh
+
+                                     deadlineNotReached :: Bool
+                                     deadlineNotReached = contains (to $ _deadline) $ txInfoValidRange info
 
 {-# INLINABLE mkWrappedDeadlinePolicy #-}
 mkWrappedDeadlinePolicy :: PubKeyHash -> POSIXTime -> BuiltinData -> BuiltinData -> ()
