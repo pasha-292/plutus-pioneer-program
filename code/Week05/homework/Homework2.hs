a83940f56e27621293e3ac739413cf3f6d0c3923
Md Juhaer Adittya Pasha <juhaeradittya2000@gmail.com>
2023-07-07 14:25:12 +0000

    Homework 2 Week05 pass most tests

--- code/Week05/homework/Homework2.hs
+++ code/Week05/homework/Homework2.hs
@@ -5,18 +5,45 @@
 
 module Homework2 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy,
-                                       ScriptContext, TokenName, TxOutRef,
-                                       mkMintingPolicyScript)
+import qualified Data.ByteString.Char8      as BS8
+import           Plutus.V1.Ledger.Value     (flattenValue)
+import           Plutus.V2.Ledger.Api       (BuiltinData, CurrencySymbol,
+                                             MintingPolicy,
+                                             ScriptContext (scriptContextTxInfo),
+                                             TokenName (unTokenName),
+                                             TxId (TxId, getTxId),
+                                             TxInInfo (txInInfoOutRef),
+                                             TxInfo (txInfoInputs, txInfoMint),
+                                             TxOutRef (TxOutRef, txOutRefId, txOutRefIdx),
+                                             mkMintingPolicyScript)
 import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (False), ($), (.))
-import           Utilities            (wrapPolicy)
+import           PlutusTx.Builtins.Internal (BuiltinByteString (BuiltinByteString))
+import           PlutusTx.Prelude           (Bool (False), Eq ((==)), any,
+                                             traceIfFalse, ($), (&&), (.))
+import           Prelude                    (IO, Show (show), String)
+import           Text.Printf                (printf)
+import           Utilities                  (bytesToHex, currencySymbol,
+                                             wrapPolicy, writeCodeToFile,
+                                             writePolicyToFile)
+import           Data.String                                             
 
 {-# INLINABLE mkEmptyNFTPolicy #-}
 -- Minting policy for an NFT, where the minting transaction must consume the given UTxO as input
 -- and where the TokenName will be the empty ByteString.
 mkEmptyNFTPolicy :: TxOutRef -> () -> ScriptContext -> Bool
-mkEmptyNFTPolicy _oref () _ctx = False -- FIX ME!
+mkEmptyNFTPolicy _oref () _ctx = traceIfFalse "UTxO not consumed"   hasUTxO     &&
+                          traceIfFalse "wrong amount minted" checkMintedAmount
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo _ctx
+
+    hasUTxO :: Bool
+    hasUTxO = any (\i -> txInInfoOutRef i == _oref) $ txInfoInputs info
+
+    checkMintedAmount :: Bool
+    checkMintedAmount = case flattenValue (txInfoMint info) of
+        [(_, tn, amt)] -> amt == 1
+        _                -> False
 
 {-# INLINABLE mkWrappedEmptyNFTPolicy #-}
 mkWrappedEmptyNFTPolicy :: TxOutRef -> BuiltinData -> BuiltinData -> ()
